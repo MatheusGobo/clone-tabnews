a2e372f98f32dbacf20786d06e45fc5ba9ad7321
Matheus Gobo <matheusgobo900@gmail.com>
2025-12-07 09:54:37 -0300

    Removendo Teste de Teste.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,4 @@
+// test.only Roda somente o teste com esse `.only`
 test("GET to /api/v1/status should return 200", async () => {
   // Fetch similar ao curl
   const response = await fetch("http://localhost:3000/api/v1/status")
@@ -11,8 +12,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.11");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-})
-// test.only Roda somente o teste com esse `.only`
-test("Teste de SQL Injection", async () => {
-  await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
 })

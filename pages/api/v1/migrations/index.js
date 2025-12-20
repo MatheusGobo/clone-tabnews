7a95faece27f3a53e8ac03704f84a4c1fa39389e
Matheus Gobo <matheusgobo900@gmail.com>
2025-12-20 16:22:31 -0300

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,40 +1,53 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
+import { error } from "node:console";
 
 async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const defaultMigrationOptons = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  }
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptons);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptons,
-      dryRun: false,
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" Not Allowed`
     });
+  }
 
-    await dbClient.end();
+  let dbClient;
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationOptons = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"), // Independente do Sistema Operacional ele vai fazer o caminho certo ex: "infra/migrations"
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptons);
+      return response.status(200).json(pendingMigrations);
+    }
 
-  return response.status(405).json({ message: "Method Not Allowed" });
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptons,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+  finally {
+    await dbClient.end();
+  }
 }
 export default migrations;
